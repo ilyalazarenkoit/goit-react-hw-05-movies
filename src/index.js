d568f702fabb2d7c11cd79ca63e36aecfe494ee0
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-12-27 04:10:28 +0200

    first commit before check

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Global } from 'GlobalStyles.styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="goit-react-hw-05-movies">
       <Global />
       <App />
     </BrowserRouter>
